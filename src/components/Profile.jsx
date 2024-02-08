78c1db83ad0a7a1b61a90348eb37d17c995393f5
Pavel <Pavel.Pasechnik@gmail.com>
2024-02-08 14:01:45 +0200

    task 1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+// src/components/Product.jsx
+import styles from './css/Profile.module.css';
+
+export const Profile = ({ name, tag, location, image, stats: { followers, views, likes } }) => {
+  return (
+    <article>
+      <div className={styles.card}>
+        <div className={styles.photo}>
+          <img className={styles.avatar} src={image} alt='User avatar' />
+        </div>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.rating}>
+        <li className={styles.ratingItem}>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li className={styles.ratingItem}>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li className={styles.ratingItem}>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </article>
+  );
+};
